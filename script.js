bfd5148e9a7f022d1e3eae1dd2b4a14663121caf
clementomondi <clementomondi2004@gmail.com>
2023-08-27 17:51:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var stars = document.querySelectorAll('.star a');
+
+stars.forEach((item, index1) => {
+    item.addEventListener('click', () => {
+        stars.forEach((star, index2) => {
+            index1 >= index2 ? star.classList.add('active') : star.classList.remove('active')
+        })
+    })
+})
+
+
+const nchors = document.querySelectorAll("a");
+
+anchors.forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault()
+    })
+})
